699bead696470d7c0e23b4d3a9585e0598f350f9
amirmoussa01 <moussaamir979@gmail.com>
2026-01-22 16:53:15 +0100

    Icone de telechargemment

---
+++ static/service-worker.js
@@ -0,0 +1,15 @@
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open('agritech-cache').then(cache => {
+      return cache.addAll([
+        '/',
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    fetch(event.request).catch(() => caches.match(event.request))
+  );
+});
